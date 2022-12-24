2ff4445453242ea128e307505ff74adcc79f2b9f
Huy Tran <huytran172@gmail.com>
2022-12-24 13:24:28 -0500

    fix: Asset path

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
    * Tell Next.js where the `public` folder is.
    * Replace `nextjs-github-pages` with your Github repo project name.
    */
-  assetPrefix: isProd ? "/max172-hqt.github.io/" : undefined,
+  assetPrefix: undefined,
   /**
    * Disable server-based image optimization.
    *
