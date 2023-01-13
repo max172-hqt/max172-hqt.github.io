50dbbcbddddd808e1676891ec5d2fdad35652eec
Huy Tran <huytran172@gmail.com>
2023-01-13 13:55:01 -0500

    feat: Add GG analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,34 @@
-import 'github-markdown-css/github-markdown-light.css'
-import 'highlight.js/styles/default.css'
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import Layout from '../components/Layout/layout'
+import "github-markdown-css/github-markdown-light.css";
+import "highlight.js/styles/default.css";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Layout from "../components/Layout/layout";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-CRQQ6MWGP0"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-CRQQ6MWGP0', {
+              page_path: window.location.pathname,
+              });
+            `,
+        }}
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
